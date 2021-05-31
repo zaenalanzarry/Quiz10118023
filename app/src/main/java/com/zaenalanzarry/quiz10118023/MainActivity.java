35d771daec9779f3c9a1fa0c2911fbff2ac17095
zaenalanzarry <zaenal7a@gmail.com>
2021-05-31 13:51:51 +0700

    Initial commit

---
+++ app/src/main/java/com/zaenalanzarry/quiz10118023/MainActivity.java
@@ -0,0 +1,20 @@
+package com.zaenalanzarry.quiz10118023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+/*
+    Tanggal Pengerjaan : 31 Mei 2021
+    NIM   : 10118023
+    Nama  : Zaenal Anzarry
+    Kelas : IF-1/S1/VI
+
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
